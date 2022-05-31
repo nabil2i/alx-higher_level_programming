53f6beceea8e464b5a652a03c747d70218105bf2
Nabil Affo <nabil.noble01@gmail.com>
2022-05-31 18:56:40 +0000

    Add 101 and 102

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
-
+def remove_char_at(str, n):
+    str2 = ''
+    i = 0
+    for j in str:
+        if i != n:
+            str2 = str2 + str[i]
+        i =  i + 1
+    return str2
