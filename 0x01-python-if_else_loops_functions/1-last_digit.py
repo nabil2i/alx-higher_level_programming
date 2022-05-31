bc2bf4fd72c7fda3ecc846533a866d8e5af66bba
Nabil Affo <nabil.noble01@gmail.com>
2022-05-31 16:31:42 +0000

    Solve #1 for negative numbers

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+if number < 0:
+    number = -number
 print(f"Last digit of {number:d} is {number % 10}", end = ' ')
 if number % 10 > 5:
     print("and is greater than 5")
