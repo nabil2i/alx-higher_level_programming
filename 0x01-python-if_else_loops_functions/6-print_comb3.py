ebad9c809d33b0265e32cc2f3000de4ec63b49bf
Nabil Affo <nabil.noble01@gmail.com>
2022-05-31 17:11:13 +0000

    Solve #6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(9):
     for j in range(i + 1, 10):
         if i == 8:
-            print(f"{i:d}{j:d}")
+            print("{:d}{:d}".format(i, j))
         else:
-            print(f"{i:d}{j:d}", end = ', ')
+            print("{:d}{:d}".format(i, j), end=', ')
