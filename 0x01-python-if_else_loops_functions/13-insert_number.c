b11fcf67ca34cea09ca4f960e99eb669b8051f22
Nabil Affo <nabil.noble01@gmail.com>
2022-05-31 18:00:19 +0000

    Add 100# and 13#

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * insert_node - inserts a number into a sorted singly lined list
+ * @head: list
+ * @number: number to insert
+ * Return: address of the new node or NULL if failed
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+
+
+
+}
