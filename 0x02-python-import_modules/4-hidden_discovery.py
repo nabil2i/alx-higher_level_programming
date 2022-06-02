9e0bf025bb9e5ad23e57abe7a142a4b9164f9f2a
Nabil Affo <nabil.noble01@gmail.com>
2022-06-02 22:38:44 +0000

    Modify 4#

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     from hidden_4 import *
     names = dir()
-    for i in range(0, len(arr)):
+    for i in range(0, len(names)):
         if names[i][0:2] != "__":
             print("{}".format(names[i]))
