c7685e9ccfd2d8eb18bee4d2aaf48d01cd1650bd
Nabil Affo <nabil.noble01@gmail.com>
2022-06-06 12:05:03 +0000

    Add 9#

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,7 +4,7 @@ def max_integer(my_list=[]):
         i, maxInt = 0, my_list[0]
         while i < len(my_list):
             if (my_list[i] > maxInt):
-                    maxInt = my_list[i]
+                maxInt = my_list[i]
             i += 1
         return maxInt
     else:
