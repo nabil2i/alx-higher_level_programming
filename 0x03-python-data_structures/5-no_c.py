5569027f00dd602ee1433ead787a02cc03a7cb55
Nabil Affo <nabil.noble01@gmail.com>
2022-06-06 08:46:30 +0000

    Add 5#

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    if my_string:
-        my_string2 = my_string.copy()
-        i = 0
-        while i < len(my_string2):
-            if my_string2[i] == 'c' or my_string2[i] == 'C':
-                my_string2 = my_string2[:i] + my_string2[i + 1:]
-            i += 1
-        return my_string2
+    my_string2 = my_string.translate({ord(i): None for i in 'cC'})
+    return my_string2
