99f59c8af449271561d17d135c165bd3c58023d9
Nabil Affo <nabil.noble01@gmail.com>
2022-06-08 21:21:30 +0000

    Modify 12

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,4 +2,5 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    return max(a_dictionary)
+    max_val = max(a_dictionary.keys(), key=(lambda new_k: a_dictionary[new_k]))
+    return
--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if type(roman_string) != str or roman_string == None:
+    if type(roman_string) != str or roman_string is None:
         return 0
     table = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     roman_int = 0
     for idx in range(len(roman_string)):
         if idx > 0 and table[roman_string[idx]] > table[roman_string[idx - 1]]:
-                roman_int += table[roman_string[idx]] -\
+            roman_int += table[roman_string[idx]] -\
                                 2 * table[roman_string[idx - 1]]
         else:
             roman_int += table[roman_string[idx]]
