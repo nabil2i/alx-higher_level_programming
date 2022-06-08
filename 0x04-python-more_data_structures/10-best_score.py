fc8c919c1b31cb00d9c5ebee0998cb861be562f1
Nabil Affo <nabil.noble01@gmail.com>
2022-06-08 21:26:01 +0000

    Modify 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,5 +2,6 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    max_val = max(a_dictionary.keys(), key=(lambda new_k: a_dictionary[new_k]))
-    return
+    v = list(a_dictionary.values())
+    k = list(a_dictionary.keys())
+    return k[v.index(max(v))]
