a459232f279d8f0bb8c21f83b43ebb01704b760d
Nabil <nabil.noble01@gmail.com>
2022-06-20 13:27:10 +0000

    Add 103#

---
+++ 0x05-python-exceptions/103-python.c
@@ -0,0 +1,17 @@
+#include <Python.h>
+#include <stdlib.h>
+
+void print_python_list(PyObject *p)
+{
+
+}
+
+void print_python_bytes(PyObject *p)
+{
+
+}
+
+void print_python_float(PyObject *p)
+{
+
+}
