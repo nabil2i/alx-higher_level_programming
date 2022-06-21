caf97a31efc9e3e2501b04e207274310a94f2de5
Nabil <nabil.noble01@gmail.com>
2022-06-21 22:28:51 +0000

    Add 103#

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Magic class from Python bytecode"""
+import math
+
+
+class MagicClass:
+    """Initialization of the class"""
+    def __init__(self, radius=0):
+        """Initialization of data"""
+        self.MagicClass__radius = 0
+        if not isinstance(radius, int) and not isinstance(radius, float):
+            raise TypeError("radius must be a number")
+        self._MagicClass__radius = radius
+
+    def areaa(self):
+        """Computes the area"""
+        return self._MagicClass__radius ** 2 * math.pi
+
+    def circumference(self):
+        """Computes the circumference"""
+        return 2 * math.pi * self._MagicClass__radius
