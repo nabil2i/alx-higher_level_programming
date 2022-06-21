2b0f11b0481b735d943c5deb4657fae368e56d25
Nabil <nabil.noble01@gmail.com>
2022-06-21 20:43:29 +0000

    Add 1#

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Defines a class Suare"""
+
+
+class Square:
+    """Defines a square
+
+    Attributes:
+        __size (int): size of a side of the square
+    """
+    def __init__(self, size):
+        """Initializes a square
+
+        Args:
+            size (int): size of a side of the square
+
+            Returns: None
+        """
+        self.__size = size
