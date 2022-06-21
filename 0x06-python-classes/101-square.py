7a26c50f4031593fb7b0605f6a0fb673ba77517e
Nabil <nabil.noble01@gmail.com>
2022-06-21 22:17:47 +0000

    Add 101#

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -22,6 +22,19 @@ class Square:
         self.size = size
         self.position = position
 
+    def __str__(self):
+        """Str method for print from main module."""
+        my_str = ""
+        if self.__size == 0:
+            return ''
+        else:
+            my_str += '\n' * self.__position[1]
+            for i in range(0, self.__size):
+                my_str += ' ' * self.__position[0]
+                my_str += '#' * self.__size
+                my_str += '\n'
+            return my_str[:-1]
+
     @property
     def size(self):
         """getter of __size
