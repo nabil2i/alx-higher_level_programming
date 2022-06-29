6f871bbf0aa2c90c59e17fa55bd61db14aa3111b
Nabil <nabil.noble01@gmail.com>
2022-06-29 21:52:54 +0000

    Modify 100#

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -2,3 +2,5 @@
 class LockedClass:
     """User can only create instance attribute 'first_name'"""
     __slots__ = ['first_name']
+    def __init__(self, first_name=''):
+        self.first_name = first_name
