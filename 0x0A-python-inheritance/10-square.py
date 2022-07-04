066659d5a2294f17b6a8631bcb89236026312fa2
Nabil <nabil.noble01@gmail.com>
2022-07-04 19:04:47 +0000

    Add 10#

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Class BaseGeometry"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Class Square
+
+    Attributes:
+        __size (int): size of the square
+        """
+
+    def __init__(self, size):
+        """Initializes an instance"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """Computes the area of the rectanglge"""
+        return self.__size ** 2
