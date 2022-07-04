6bf5d6bac79cb43acfdcb9457ed388bfd116905f
Nabil <nabil.noble01@gmail.com>
2022-07-04 17:47:11 +0000

    Add 6#

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Class BaseGeometry"""
+
+
+class BaseGeometry:
+    """Class BaseGeometry"""
+
+    def __init__(self):
+        """Initializes an instance"""
+
+    def area(self):
+        """Raises an exception"""
+        raise Exception("area() is not implemented")
