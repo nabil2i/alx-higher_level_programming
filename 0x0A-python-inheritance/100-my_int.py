ebfe5e933734a0ef97bb7573cdabea865659f801
Nabil <nabil.noble01@gmail.com>
2022-07-04 19:24:56 +0000

    Add 100#

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Class MyInt"""
+
+
+class MyInt(int):
+    """class MyInt"""
+    def __new__():
+        """new instance oef the class"""
+        return super(MyInt, cls).__new__(cls, *args, **kwargs)
+
+    def __eq__(self, other):
+        """!= becomes =="""
+        return int(self) != other
+
+    def __ne__(self, other):
+        """== becomes !="""
+        return int(self) == other
