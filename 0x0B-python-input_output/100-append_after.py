9c643ed4923d741fac3487c895e5a90ccdc2fe47
Nabil <nabil.noble01@gmail.com>
2022-07-05 19:16:53 +0000

    Add 12# 100# and 101#

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Function append_after
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """Inserts a line of text to a file, after each line containing
+    a specific string
+    """
+    with open(filename, "r", encoding="utf-8") as f:
+        lines = f.readlines()
+
+    with open(filename, "w", encoding="utf-8") as f2:
+        my_str = ""
+        for line in lines:
+            my_str += line
+            if line.index(search_string):
+                my_str += new_string
+        f2.write(my_str)
---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3i
+"""Function pascal_triangle
+"""
+
+
+def pascal_triangle(n):
+    """returns a list of integers representing the
+    Pascal's triangle of n
+    """
+    my_list = []
+    if n <= 0:
+        return my_list
+
+    my_list = [[0 for x in range(i + 1] for i in range(n)]
+    return my_list
