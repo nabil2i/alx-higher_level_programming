af4059b22c2ccf80bf1b3d36ec4bb29d2a510ce7
Nabil <nabil.noble01@gmail.com>
2022-07-05 18:50:29 +0000

    Add 11#

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Class Student"""
+
+
+class Student:
+    """Defines a student
+    Attributes:
+        first_name (string):
+        last_name (string):
+        age (int):
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes an instance of Student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of
+        a Student instance with specific attributes
+        """
+        if attrs is None:
+            return self.__dict__
+        new_dict = {}
+        for attr in attrs:
+            try:
+                new_dict[attr] = self.__dict__[attr]
+            except (Exception):
+                pass
+        return new_dict
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student instance
+        """
+        for attr in json:
+            try:
+                setattr(self, attr, json[attr])
+            except (Exception):
+                pass
