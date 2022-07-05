86a8d86a30e4a7a55a7d18718f269faefccb50d0
Nabil <nabil.noble01@gmail.com>
2022-07-05 17:04:06 +0000

    Fix 0#

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,5 +6,5 @@ def read_file(filename=""):
     """Reads a text file with utf8 encoding
     and prints it to stdout
     """
-    with open(filename, encoding='utf-8') as f:
-        print(f.read())
+    with open(filename, "r", encoding='utf-8') as f:
+        print(f.read(), end='')
