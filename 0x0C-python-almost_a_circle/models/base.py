2d5269c3ed24f12faa24228aff3ce6cb631ebcc9
Nabil <nabil.noble01@gmail.com>
2022-07-11 23:07:01 +0000

    base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,6 +3,10 @@
 Base class for of all other classes
 """
 
+import json
+import os
+import csv
+
 
 class Base:
     """class Base
@@ -25,3 +29,13 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns the JSON string representation
+        of list_dictionaries
+        """
+
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
