8080e9a3977deac1518b6dc38245091ae03bfafe
Nabil <nabil.noble01@gmail.com>
2022-07-11 21:25:11 +0000

    Modify rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -114,3 +114,18 @@ class Rectangle(Base):
 
         return "[Rectangle] ({}) {}/{} - {}/{}".format(
                 self.id, self.__x, self.__y, self.__width, self.__height)
+
+    def update(self, *args):
+        """assigns an argument to each attribute"""
+
+        if args is not None and len(args) != 0:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) > 1:
+                self.width = arg[1]
+            if len(args) > 2:
+                self.height = arg[2]
+            if len(args) > 3:
+                self.x = arg[3]
+            if len(args) > 4:
+                self.y = arg[4]
