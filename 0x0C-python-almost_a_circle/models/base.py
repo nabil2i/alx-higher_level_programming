d7366cd11dfdaefa4edc86c830a8fefb41ee8754
Nabil <nabil.noble01@gmail.com>
2022-07-11 19:07:52 +0000

    Create execfiles

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module base
+Base class for of all other classes
+"""
+
+
+class Base:
+    """class Base
+
+    Attributes:
+        __nb_objects (int): private class attribute
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initialiazes an instance of Base
+
+        Args:
+            id (int): id of an instance of Base
+        """
+
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_object
