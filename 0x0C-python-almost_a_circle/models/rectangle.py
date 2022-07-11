e5c53483e1d415067b06ed9da2389967cd475b5c
Nabil <nabil.noble01@gmail.com>
2022-07-11 20:36:05 +0000

    Validation for rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -46,19 +46,39 @@ class Rectangle(Base):
         @width.setter
         def width(self, value):
             """Setter of __width"""
+
+            if type(value) is not int:
+                raise TypeError("width must be an integer")
+            if value <= 0:
+                raise ValueError("width must be > 0")
             self.__width = value
 
         @height.setter
         def height(self, value):
             """Setter of __height"""
+
+            if type(value) is not int:
+                raise TypeError("height must be an integer")
+            if value <= 0:
+                raise ValueError("height must be > 0")
             self.__height = value
 
         @x.setter
         def x(self, value):
             """Setter of __x"""
+
+            if type(value) is not int:
+                raise TypeError("x must be an integer")
+            if value < 0:
+                raise ValueError("x must be >= 0")
             self.__x = value
 
         @y.setter
         def y(self, value):
             """Setter of __y"""
+
+            if type(value) is not int:
+                raise TypeError("x must be an integer")
+            if value < 0:
+                raise ValueError("y must be >= 0")
             self.__y = value
