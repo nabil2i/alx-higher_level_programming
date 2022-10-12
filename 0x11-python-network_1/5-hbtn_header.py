a3208d086bed4cba5c61e2ca63310b598e5c0d2c
Nabil <nabil.noble01@gmail.com>
2022-10-12 14:08:27 +0000

    Edit

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Takes in a URL, sends a request to the URL and displays the value of the
+""" script takes in a URL, sends a request to the URL and displays the value of the
 X-Request-Id variable found in the header of the response"""
 
 
