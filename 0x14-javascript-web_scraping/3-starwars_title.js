57b4eb3d541b46468d756d663bab2174dfc12f8e
Nabil <nabil.noble01@gmail.com>
2022-10-19 22:00:09 +0000

    Edit

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,13 +1,15 @@
 #!/usr/bin/node
 
 const request = require('request');
-
-request('http://swapi.co/api/films/' + process.argv[2], function (err, response, body) {
+let url = 'http://swapi.co/api/films/';
+let episode = process.argv[2];
+request(url + episode, function (err, response, body) {
   if (err) {
-    console.error(err);
+    console.log(err);
   } else if (response.statusCode === 200) {
-    console.log(JSON.parse(body).title);
+    let jsonobj = JSON.parse(body);
+    console.log(jsonobj.title);
   } else {
-    console.log('Error. Status code: ' + response.statusCode);  
+    console.log('An error occured. Status code: ' + response.statusCode);
   }
 });
