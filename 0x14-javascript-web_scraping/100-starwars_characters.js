0d79557246a99b986cef1db9d743315887802c5b
Nabil <nabil.noble01@gmail.com>
2022-10-19 22:23:15 +0000

    Edit

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,17 +1,27 @@
 #!/usr/bin/node
-
 const request = require('request');
-
-request('http://swapi.co/api/films/' + process.argv[2], function (error, response, body) {
-  if (error) {
-    console.error(error);
-  }
-  JSON.parse(body).characters.forEach(function (url, callback) {
-    request(url, function (error, response, body) {
-      if (error) {
-        console.error(error);
+const film = process.argv[2];
+let url = 'http://swapi.co/api/people/';
+function moviecharacters (film, url) {
+  request(url, function (err, response, body) {
+    if (err) {
+      console.log(err);
+    } else if (response.statusCode === 200) {
+      let jsonobj = JSON.parse(body);
+      let characters = jsonobj.results;
+      for (let i in characters) {
+        for (let j in characters[i].films) {
+          if (characters[i].films[j].includes(film)) {
+            console.log(characters[i].name);
+          }
+        }
       }
-      console.log(JSON.parse(body).name);
-    });
+      if (jsonobj.next !== null) {
+        moviecharacters(film, jsonobj.next);
+      }
+    } else {
+      console.log('Error. Status code: ' + response.statusCode);
+    }
   });
-});
+}
+moviecharacters(film, url);
